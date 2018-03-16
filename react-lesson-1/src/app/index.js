cbb80f0a496dd8c8af94e7b3961eb6746e794c05
olzhas2102 <olzhas2102@yahoo.com>
2018-03-16 21:38:19 +0100

    Props

--- react-lesson-1/src/app/index.js
+++ react-lesson-1/src/app/index.js
@@ -6,12 +6,15 @@ var TodoComponent = React.createClass({
   render: function() {
     return(
       <div>
-        <h1>Bonjour</h1>
-        <p>Halo</p>
+        <p><strong>Halva name:</strong>{this.props.halva.name}</p>
+        <p><strong>Halva smell factor:</strong>{this.props.halva.smellFactor}</p>
+        <p><strong>Halva price:</strong>{this.props.halva.price}</p>
       </div>
     );
   }
 });
 
+var myHalva = {name: 'Rodina', smellFactor: 'Delicio', price: '3.50'};
+
 // Put component into HTML page 
-ReactDOM.render(<TodoComponent/>, document.getElementById('todo-wrapper'));
+ReactDOM.render(<TodoComponent mssg="I like halva" halva={myHalva}/>, document.getElementById('todo-wrapper'));

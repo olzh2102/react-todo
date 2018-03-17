f096231ad8ca1ff0566288a80ec9dc5d8920853d
olzhas2102 <olzhas2102@yahoo.com>
2018-03-17 10:24:22 +0100

    Updating data via virtual DOM

--- react-lesson/src/app/index.js
+++ react-lesson/src/app/index.js
@@ -5,14 +5,21 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   getInitialState: function() {
     return {
-      todos: ['wash up', 'help friend', 'take a nap']
+      todos: ['wash up', 'help friend', 'take a nap'],
+      age: 30
     }
   },
 
   render: function() {
+    var ager = setTimeout(() => {
+      this.setState({
+        age: 35
+      });
+    }, 5000);
     return(
       <div id="todo-list">
         <p>The busiest people have the most leisure...</p>
+        <p>{this.state.age}</p>
         <ul>
           <li>{this.state.todos[0]}</li>
           <li>{this.state.todos[1]}</li>

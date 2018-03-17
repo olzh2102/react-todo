0313e20b3c5307d0f7157f6e56dd2c23bee075cf
olzhas2102 <olzhas2102@yahoo.com>
2018-03-17 10:31:11 +0100

    Cycling through a data

--- react-lesson/src/app/index.js
+++ react-lesson/src/app/index.js
@@ -5,26 +5,21 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
   getInitialState: function() {
     return {
-      todos: ['wash up', 'help friend', 'take a nap'],
-      age: 30
+      todos: ['wash up', 'help friend', 'take a nap', 'buy a milk']
     }
   },
 
   render: function() {
-    var ager = setTimeout(() => {
-      this.setState({
-        age: 35
-      });
-    }, 5000);
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index) {
+      return (
+        <li>{item}</li>
+      );
+    });
     return(
       <div id="todo-list">
         <p>The busiest people have the most leisure...</p>
-        <p>{this.state.age}</p>
-        <ul>
-          <li>{this.state.todos[0]}</li>
-          <li>{this.state.todos[1]}</li>
-          <li>{this.state.todos[2]}</li>
-        </ul>
+        <ul>{todos}</ul>
       </div>
     );
   } // render 
